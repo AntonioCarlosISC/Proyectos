61fa2b4ab98648838485fb57d41b536dac3ffc06
AntonioCarlosISC <antoniocarlosrd.isc@gmail.com>
2025-10-28 13:42:53 -0600

    Se agregó compilación condicional para soporte multiplataforma

--- RPGMotor/app/game_loop.c
+++ RPGMotor/app/game_loop.c
@@ -1,12 +1,23 @@
-//#region Encabezados
-#include <SDL2/SDL_stdinc.h>
-#include <SDL2/SDL_video.h>
+//#region Encabezados portables
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <SDL2/SDL.h>
 #include <pthread.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+    // Windows
+    #include <SDL2/SDL.h>
+#elif defined(__APPLE__) && defined(__MACH__)
+    // macOS
+    #include <SDL2/SDL.h>   // Asumiendo que usarás el framework de SDL2 o include path de Homebrew
+#else
+    // Linux / otros UNIX
+    #include <SDL2/SDL_stdinc.h>
+    #include <SDL2/SDL_video.h>
+    #include <SDL2/SDL.h>
+#endif
 //#endregion
+
 
 //#region Prototipos
 int game_loop(void);
